23c609f2f5ac71c76f696260371eeb856f933110
Ammar <muhdammar970320@gmail.com>
2021-10-11 08:38:17 +0800

    update logic

--- CalculatorIdeagenTechnicalTest/Program.cs
+++ CalculatorIdeagenTechnicalTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CalculatorIdeagenTechnicalTest
 {
@@ -7,9 +8,7 @@ namespace CalculatorIdeagenTechnicalTest
     {
         static void Main(string[] args)
         {
-            var expression = "1 + 1";
-            var test = expression.Split(" ");
-            Console.WriteLine(test);
+            var expression = "1 + 1 + 2";
             Console.WriteLine(Calculate(expression));
         }
         /// <summary>
@@ -17,9 +16,7 @@ namespace CalculatorIdeagenTechnicalTest
         /// Can use tree algorithm or stack but need to rearrange the equation to prefix/postfix first
         /// Comment: since this one im not remember how to use it and i cant googling it,
         /// i think i wanna try design other logic using pseudocode.
-        /// Important!!!
-        /// HackerRank profile: https://www.hackerrank.com/ammar_aziz
-        /// i got used HackerRank to test my logical think, maybe can review if u free
+        /// HackerRank profile: https://www.hackerrank.com/ammar_azi
         /// </summary>
         /// <param name="expression"></param>
         /// <returns></returns>
@@ -38,37 +35,113 @@ namespace CalculatorIdeagenTechnicalTest
             //TODO Test Case 2
             // 1 x 4 - 4 + 6 / 3 - 3 + 7 (6 symbols = 6 steps)
             //Step 1: (1 x 4) - 4 + 6 / 3 - 3 + 7
-            //Step 2: 4 - 4 + 6 / 3 - 3 + 7
-            //Step 3: 4 - 4
-            //Step 4: 3 - 3
-            //Answer: 0
-            var items = expression.Split(" ");
-
-            foreach (var item in items)
+            //Step 2: 4 - 4 + (6 / 3) - 3 + 7
+            //Step 3: (4 - 4) + 2 - 3 + 7
+            //Step 4: 0 + (2 - 3) + 7
+            //Step 5: 0 + (-1 + 7)
+            //Step 6: 0 + 6
+            //Answer: 6
+            
+            //TODO Test Case 3
+            // 1 x 7 - 4 - 6 / 2 + 2 (5 symbols = 5 steps)
+            //Step 1: (1 x 7) - 4 - 6 / 2 + 2
+            //Step 2: 7 - 4 - (6 / 2) + 2
+            //Step 3: (7 - 4) - 3 + 2
+            //Step 4: (3 - 3) + 2
+            //Step 5: 0 + 2
+            //Answer: 2
+            
+            //Pseudocode
+            //1. Count steps, each symbol assume 1 step
+            //2. Finish multiply or divide first
+            //3. finish subtract and addition
+            
+            var items = expression.Split(" ").ToList();
+            var steps = items.Count / 2;
+            
+            for (int i = 0; i < steps; i++)
             {
-                switch (item)
+                for (int j = 0; j < items.Count; j++)
                 {
-                    case "+":
-                        // code block
-                        break;
-                    case "-":
-                        // code block
-                        break;
-                    case "/":
-                        // code block
-                        break;
-                    case "*":
-                        // code block
-                        break;
-                    default:
-                        var number = double.Parse(item);
-                        numbers.Add(number);
-                        break;
+                    if (items[j] == "x" )
+                    {
+                        // 1 x 3
+                        //take index before and after "x" and calculate
+                        var index = j;
+                        var x = Double.Parse(items[j - 1]);
+                        var y = Double.Parse(items[j + 1]);
+                        var calculate = x * y;
+                        items.Insert(j - 1, calculate.ToString());
+                        items.RemoveAt(j);
+                        items.RemoveAt(j+1);
+                    }
+
+                    if (items[j] == "/")
+                    {
+                        var index = j;
+                        var x = Double.Parse(items[j - 1]);
+                        var y = Double.Parse(items[j + 1]);
+                        var calculate = x / y;
+                        items.Insert(j - 1, calculate.ToString());
+                        items.RemoveAt(j);
+                        items.RemoveAt(j+1);
+                    }
+                    
+                    if (items[j] == "+" )
+                    {
+                        // 1 x 3
+                        //take index before and after "x" and calculate
+                        var index = j;
+                        var x = Double.Parse(items[j - 1]);
+                        var y = Double.Parse(items[j + 1]);
+                        var calculate = x + y;
+                        items.Insert(j - 1, calculate.ToString());
+                        items.RemoveAt(j);
+                        items.RemoveAt(j+1);
+                    }
+                    
+                    if (items[j] == "-" )
+                    {
+                        // 1 x 3
+                        //take index before and after "x" and calculate
+                        var index = j;
+                        var x = Double.Parse(items[j - 1]);
+                        var y = Double.Parse(items[j + 1]);
+                        var calculate = x - y;
+                        items.Insert(j - 1, calculate.ToString());
+                        items.RemoveAt(j);
+                        items.RemoveAt(j+1);
+                    }
                 }
             }
+            if (items.Count==2)
+            {
+                return Double.Parse(items[0]);
+            }
+            // foreach (var item in items)
+            // {
+            //     switch (item)
+            //     {
+            //         case "+":
+            //             // code block
+            //             break;
+            //         case "-":
+            //             // code block
+            //             break;
+            //         case "/":
+            //             // code block
+            //             break;
+            //         case "*":
+            //             // code block
+            //             break;
+            //         default:
+            //             var number = double.Parse(item);
+            //             numbers.Add(number);
+            //             break;
+            //     }
+            // }
 
-            var a = 1;
-            return 10;
+            return -1;
         }
     }
 }
